3931538c24063a37be8386918b90da0e3f43a9e1
unknown <achraf.rehioui.1@gmail.com>
2020-02-14 11:51:10 +0100

    added cinemacomponent and app-routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CinemaComponent } from './cinema/cinema.component';
+import {Routes, RouterModule} from '@angular/router';
+
+const routes: Routes =[
+  {
+    path: "cinema",
+    component: CinemaComponent
+  }
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CinemaComponent } from './cinema/cinema.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CinemaComponent
   ],
+ 
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
